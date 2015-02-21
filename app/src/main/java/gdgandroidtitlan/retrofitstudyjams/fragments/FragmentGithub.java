cc6acedcf9b8373a3838eea7e16e48191f9aaf6e
Jhordan <erik_inho109@hotmail.com>
2015-02-21 00:50:59 -0600

    Study Jams - android

--- app/src/main/java/gdgandroidtitlan/retrofitstudyjams/fragments/FragmentGithub.java
+++ app/src/main/java/gdgandroidtitlan/retrofitstudyjams/fragments/FragmentGithub.java
@@ -77,8 +77,6 @@ public class FragmentGithub extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-
     }
 
 
